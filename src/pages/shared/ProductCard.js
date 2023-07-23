14358c1e93e7f85a56ad24e599309bb3e190cd61
hasibul1670 <hasibulislam1670@gmail.com>
2023-07-23 19:52:35 +0600

    feat : category page Implemented

--- src/pages/shared/ProductCard.js
+++ src/pages/shared/ProductCard.js
@@ -2,30 +2,30 @@ import Image from "next/image";
 import Link from "next/link";
 
 const ProductCard = ({ product }) => {
-  console.log("Hello", product);
-  const { name,_id, price, image_url, category, stock, currency } = product;
+
+ 
  
   return (
     <div className="card w-64 p-2 border-solid border-2 border-sky-500 shadow-xl hover:shadow-3xl transform hover:-translate-y-1 transition-all duration-300">
       <figure className="px-6 pt-2">
         <Image
           alt="example"
-          className="bg-red-500"
-          src={image_url}
+          src={product?.image_url}
           height={200}
           width={200}
           responsive
         />
       </figure>
       <div className="card-body items-center text-center py-3">
-        <h6 className="font-bold text-cyan-700 ">{name}</h6>
-        <p className=" text-cyan-700 ">Category:{category}</p>
-        <p className="text-bold text-cyan-400">Stock Available:{stock}</p>
+        <h6 className="font-bold text-cyan-700 ">{product?.name}</h6>
+        <p className=" text-cyan-700 ">Category:{product?.category}</p>
+        <p className=" text-cyan-700 font-bold ">Brand:{product?.brand}</p>
+        <p className="text-bold text-cyan-400">Stock Available:{product?.stock}</p>
         <h6 className="font-bold  ">
-          {price} {currency}
+          {product?.price} {product?.currency}
         </h6>
 
-        <Link href={`/products/${_id}`}>
+        <Link href={`/products/${product?._id}`}>
           <button className="btn  btn-primary  btn-sm">Show Detail</button>
         </Link>
       </div>
